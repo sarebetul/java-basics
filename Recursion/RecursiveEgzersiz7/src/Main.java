d354e28c1a4a241e405e060ca55ac15b404d2d05
sarebetul <yazarsarebetul@gmail.com>
2026-02-23 17:35:28 +0300

    Remove old RecursiveEgzersiz6 and 7 folders

--- Recursion/RecursiveEgzersiz7/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static int obeb(int a , int b){
-    if(b==0) return a;
-    else return obeb(b,a%b);
-  }
-  public static void main(String[] args){
-    System.out.println(obeb(48,24));
-  }
-}
