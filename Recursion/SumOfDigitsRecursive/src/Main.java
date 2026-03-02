d38bc16686f1739bf531051917bb9ec3fb20a2c2
sarebetul <yazarsarebetul@gmail.com>
2026-03-02 07:12:20 +0300

    Update SumOfDigitsRecursive implementation

--- Recursion/SumOfDigitsRecursive/src/Main.java
+++ Recursion/SumOfDigitsRecursive/src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static int digit(int n){
         if(n<10) return n;
-        int top = 0;
-        top = top + (n%10);
         return n%10 + digit(n/10);
     }
     public static void main(String[] args){
